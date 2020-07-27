2a8a8ccadb72fdb7fe51316507917bd257420051
Kosi Nwabueze <kosinwabueze@gmail.com>
2020-07-26 19:39:47 -0700

    Added resample_lenghts to Covid19MLNetworkedSim.datasets

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -7,8 +7,18 @@ import numpy as np
 import pandas as pd
 from typing import List
 
-def length_distribution(Xn):
-    return np.array([x.shape[0] for x in Xn])
+def resample_lengths(Xn: List[np.ndarray]) -> np.ndarray:
+    longest = max(Xn, key=lambda xn: xn.shape[0]).shape[0]
+    new_Xn = np.zeros((len(Xn), longest, Xn[0].shape[1]))
+
+    for k, xn in enumerate(Xn):
+        old_ax = np.linspace(0, xn.shape[0], xn.shape[0])
+        new_ax = np.linspace(0, xn.shape[0], longest)
+
+        for i in range(xn.shape[1]):
+            new_Xn[k, :, i] = np.interp(new_ax, old_ax, xn[:, i])
+
+    return new_Xn
 
 def synthetic_numpy(X: List[pd.DataFrame], y: pd.DataFrame) -> (np.ndarray, np.ndarray):
     y = y.copy()
@@ -18,9 +28,12 @@ def synthetic_numpy(X: List[pd.DataFrame], y: pd.DataFrame) -> (np.ndarray, np.n
     y['population'] = y['population'].cat.codes
     y['initial_infected'] = y['initial_infected'].cat.codes
 
-    return np.stack([x.loc[:, 'step':].to_numpy() for x in X], axis=0), y.loc[:, 'population':].to_numpy()
+    yn = y.loc[:, 'population':].to_numpy()
+    Xn = [x.loc[:, 'step':].to_numpy() for x in X]
 
-def import_synthetic(archive='synthetic-1595799389.927907.tar.gz', relative=False):
+    return Xn, yn
+
+def import_synthetic(archive='synthetic-1595799389.927907.tar.gz', relative=False) -> (pd.DataFrame, pd.DataFrame):
     fn = archive
 
     if not relative:
@@ -47,5 +60,7 @@ def import_synthetic(archive='synthetic-1595799389.927907.tar.gz', relative=Fals
         features['k']                = pd.Categorical(features['k'])
         features['population']       = pd.Categorical(features['population'])
         features['initial_infected'] = pd.Categorical(features['initial_infected'])
+
+        timeseries.index = timeseries.step
 
     return [v for k, v in timeseries.groupby(['case'])], features
